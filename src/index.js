d35c94160466cfa78922528506c08858486662d4
Konstantin Berezko <berezkok72@gmail.com>
2021-05-13 18:17:51 +0500

    new(task1): create modular structure and customize webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+import fn from './modules/test.js';
+
+fn();
---
+++ src/modules/test.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const str = 'test';
+const fn = () => {
+    console.log(`webpack ${str}`);
+};
+//fn();
+export default fn;

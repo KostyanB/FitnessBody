e41b61b0769da8a4ff37f7a0ad9ab92ddf3132fe
Konstantin Berezko <berezkok72@gmail.com>
2021-05-13 20:24:56 +0500

    new(task02): create fallout menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
 import dropMenu from './modules/falloutMenu';
+import showVisitForm from './modules/showFreeVisitForm';
 // п.2 выпадающее меню
 dropMenu();
+// п.3 показ формы записи
+showVisitForm();
--- src/modules/falloutMenu.js
+++ src/modules/falloutMenu.js
@@ -1,17 +1,35 @@
 'use strict'
 
+import animate from './animate';
 const dropMenu = () => {
     const clubSelector = document.getElementById('club-selector'),
-        falloutActivator = document.getElementById('fallout-activator');
+        falloutActivator = document.getElementById('fallout-activator'),
+        stopPosMenu = falloutActivator.getBoundingClientRect().bottom + 15;
 
-        document.addEventListener('click', (e) => {
-            if(e.target === falloutActivator) {
-                clubSelector.style.display = 'block';
-            } else if(e.target !== falloutActivator) {
-                clubSelector.style.display = 'none';
-            }
+    document.addEventListener('click', (e) => {
+        if (e.target === falloutActivator) {
+            clubSelector.style.display = 'block';
+            clubSelector.style.transform = `translateY(-100%)`;
+            menuAnim();
+        } else if (e.target !== falloutActivator) {
+            clubSelector.style.display = 'none';
+        }
+    });
+    const menuAnim = () => {
+        animate({
+            duration: 500,
+            timing: (timeFraction) => {
+                return timeFraction;
+            },
+            draw: (progress) => {
+                const menuPos = clubSelector.getBoundingClientRect().top,
+                    startPosMenu = -100,
+                    posMenu = startPosMenu + (stopPosMenu - startPosMenu) * progress;
+                if (menuPos < stopPosMenu) {
+                    clubSelector.style.transform = `translateY(${posMenu}%)`;
+                }
+            },
         });
+    };
 };
 export default dropMenu;
-
-

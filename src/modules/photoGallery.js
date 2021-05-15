9a9dde23ff3245f0ca792069d84a0b118d943267
Konstantin Berezko <berezkok72@gmail.com>
2021-05-16 04:37:27 +0500

    feat(diplom): add slider photoGallery

--- src/modules/photoGallery.js
+++ src/modules/photoGallery.js
@@ -1,48 +1,149 @@
 'use strict'
+
 const photoSlider = () => {
     const gallerySlider = document.querySelector('.gallery-slider'),
-    galleryWrapper = document.querySelector('.gallery-wrapper'),
-    photoSlides = document.querySelectorAll('.photo-slide'),
-    prevBtn = document.createElement('button'),
-    nextBtn = document.createElement('button');
-    //console.log('photoSlides: ', photoSlides);
+        photoSlides = document.querySelectorAll('.photo-slide');
 
-
-        //prevBtn.className = 'slider-dots';
-        //nextBtn.className = 'slider-dots';
-
-
-
-        //galleryWrapper.appendChild(prevBtn);
-       //galleryWrapper.appendChild(nextBtn);
-
-    for(let i = 1; i < photoSlides.length; i++) {
+    for (let i = 1; i < photoSlides.length; i++) {
         photoSlides[i].style.display = 'none';
     }
+    // установка css
+    const addClass = () => {
+        const style = document.createElement('style');
+        style.textContent = `
+            .gallery-slider {
+                position: relative;
+            }
+            .slider-arrow {
+                background: transparent;
+                border: none;
+                cursor: default !important;
+            }
+            .slider-arrow span {
+                cursor: pointer;
+            }
+            `;
+        document.head.appendChild(style);
+    };
+    addClass();
+    // добавление dot на страницу
+    const galleryDots = document.createElement('ul');
+    galleryDots.className = 'slider-dots';
+    gallerySlider.append(galleryDots);
+    const addDot = () => {
+
+        let newDot = document.createElement('li');
+        newDot.innerHTML = '<button></button>';
+        newDot.className = 'slider-dot';
+        photoSlides.forEach((item, i) => {
+            item[i] = newDot.cloneNode(true);
+            galleryDots.append(item[i]);
+        });
+        const sliderDot = document.querySelectorAll('.slider-dot');
+        sliderDot[0].classList.add('slick-active');
+    };
+    addDot();
+    // добавление кнопок на страницу
+    const addBnt = () => {
+        const prevBtn = document.createElement('button'),
+            nextBtn = document.createElement('button');
+        prevBtn.className = 'slider-arrow prev';
+        nextBtn.className = 'slider-arrow next';
+        prevBtn.innerHTML = '<span><img src="./images/arrow-left.png"></span>';
+        nextBtn.innerHTML = '<span><img src="./images/arrow-right.png"></span>';
+        gallerySlider.prepend(prevBtn);
+        gallerySlider.append(nextBtn);
+    };
+    addBnt();
+
+    const galleryPrevBtn = document.querySelector('.slider-arrow.prev'),
+        galleryNextBtn = document.querySelector('.slider-arrow.next');
+
+    // автопрокрутка слайдов
     const prevSlide = (elem, index) => {
         elem[index].style.display = 'none';
     };
-
     const nextSlide = (elem, index) => {
         elem[index].style.display = 'block';
     };
 
-    let currentSlide = 0, interval;
+    const prevDot = (elem, index, strClass) => {
+        elem[index].classList.remove(strClass);
+    };
+    const nextDot = (elem, index, strClass) => {
+        elem[index].classList.add(strClass);
+    };
+
+    let currentSlide = 0,
+        interval;
+    const sliderDot = document.querySelectorAll('.slider-dot');
+
     const playSlide = () => {
         prevSlide(photoSlides, currentSlide);
+        prevDot(sliderDot, currentSlide, 'slick-active');
         currentSlide++;
-        if(currentSlide >= photoSlides.length) {
+        if (currentSlide >= photoSlides.length) {
             currentSlide = 0;
         }
         nextSlide(photoSlides, currentSlide);
+        nextDot(sliderDot, currentSlide, 'slick-active');
     };
 
-    const timeSlider = 2000;
-    const startSlidePlay = (time = 1500) => {
-        interval = setInterval(playSlide, time);
+    const startSlidePlay = () => {
+        interval = setInterval(playSlide, 2000);
     };
-    startSlidePlay(timeSlider);
+    startSlidePlay();
 
+    const stopSlidePlay = () => {
+        clearInterval(interval);
+    };
 
+    // переключение слайдов кнопками и дотами
+    gallerySlider.addEventListener('click', (e) => {
+        if (!e.target.parentNode.matches('.slider-arrow, .slider-dot')) {
+            return;
+        }
+
+        prevSlide(photoSlides, currentSlide);
+        prevDot(sliderDot, currentSlide, 'slick-active');
+        if (e.target.closest('.next')) {
+            currentSlide++;
+        } else if (e.target.closest('.prev')) {
+            currentSlide--;
+        } else if (e.target.closest('.slider-dot')) {
+            sliderDot.forEach((elem, index) => {
+                if (elem === e.target.parentNode) {
+                    currentSlide = index;
+                }
+            });
+        }
+
+        if (currentSlide < 0) {
+            currentSlide = photoSlides.length - 1;
+        } else if (currentSlide >= photoSlides.length) {
+            currentSlide = 0;
+        }
+
+        nextSlide(photoSlides, currentSlide);
+        nextDot(sliderDot, currentSlide, 'slick-active');
+    });
+
+    // остановка прокрутки при наведении мышки на кнопки и доты
+    gallerySlider.addEventListener('mouseover', (e) => {
+        if (e.target.parentNode.classList.contains('slider-arrow') || e.target.parentNode.classList.contains('slider-dot')) {
+            stopSlidePlay();
+        }
+    });
+    galleryPrevBtn.firstChild.addEventListener('mouseleave', (e) => {
+        startSlidePlay();
+    });
+    galleryNextBtn.firstChild.addEventListener('mouseleave', (e) => {
+        startSlidePlay();
+    });
+    gallerySlider.addEventListener('mouseout', (e) => {
+        if (e.target.parentNode.className === 'slider-dot') {
+            startSlidePlay();
+        }
+    });
 };
 export default photoSlider;
--- src/modules/showForm.js
+++ src/modules/showForm.js
@@ -3,16 +3,17 @@ import animate from './animate';
 const showPopupForm = () => {
     const openFreeVisit = document.querySelector('.open-popup'),
         callbackBtn = document.querySelector('.callback-btn'),
-        giftBtn = document.querySelector('.fixed-gift').childNodes[1],
+        fixedGift = document.querySelector('.fixed-gift'),
         freeVisitForm = document.getElementById('free_visit_form'),
         callbackForm = document.getElementById('callback_form'),
-        giftForm = document.getElementById('gift'),
         formWrapper = document.querySelectorAll('.form-wrapper'),
         closefreeVisitForm = formWrapper[2].childNodes[1].childNodes[1],
         closeCallbackForm = formWrapper[1].childNodes[1].childNodes[1],
-        closeGiftForm = formWrapper[4].childNodes[1].childNodes[1],
-        giftCloseBtn = document.querySelector('.close-btn'),
         stopPosPopup = document.documentElement.clientHeight * 0.2;
+        //console.log(formWrapper);
+        //if(giftBtn) {
+            //closeGiftForm = formWrapper[4].childNodes[1].childNodes[1];
+        //}
 
     const animForm = (wrapper) => {
         animate({
@@ -36,25 +37,31 @@ const showPopupForm = () => {
         animForm(targetWrapper);
     };
     document.addEventListener('click', (e) => {
-        console.log(e.target);
-        if(e.target === openFreeVisit) {
+        if (e.target === openFreeVisit) {
             e.preventDefault();
             controlForm(freeVisitForm, formWrapper[2]);
-        } else if(e.target === closefreeVisitForm || e.target.closest('.overlay')) {
+        } else if (e.target === closefreeVisitForm || e.target.closest('.overlay')) {
             freeVisitForm.style.display = 'none';
         }
-        if(e.target === callbackBtn) {
+        if (e.target === callbackBtn) {
             controlForm(callbackForm, formWrapper[1]);
-        } else if(e.target === closeCallbackForm || e.target.closest('.overlay')) {
+        } else if (e.target === closeCallbackForm || e.target.closest('.overlay')) {
             callbackForm.style.display = 'none';
         }
-        if(e.target === giftBtn) {
-            giftBtn.style.display = 'none';
-            controlForm(giftForm, formWrapper[4]);
-        } else if(e.target === closeGiftForm || giftCloseBtn || e.target.closest('.overlay')) {
-            giftForm.style.display = 'none';
-            //giftBtn.style.display = 'inline';
+        if (fixedGift) {
+            const giftBtn = fixedGift.childNodes[1],
+            closeGiftForm = formWrapper[4].childNodes[1].childNodes[1],
+            giftForm = document.getElementById('gift'),
+            giftCloseBtn = document.querySelector('.close-btn');
+            if (e.target === giftBtn) {
+                giftBtn.style.display = 'none';
+                controlForm(giftForm, formWrapper[4]);
+            } else if (e.target === closeGiftForm || giftCloseBtn || e.target.closest('.overlay')) {
+                giftForm.style.display = 'none';
+                //giftBtn.style.display = 'inline';
+            }
         }
+
     });
 };
 export default showPopupForm;

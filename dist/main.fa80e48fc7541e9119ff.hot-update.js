b44068cfabb58f4be20f90fe46b38837263de9f7
Konstantin Berezko <berezkok72@gmail.com>
2021-05-18 05:20:06 +0500

    feat(diplom): add add slider carousel, calculator, burger-menu

---
+++ dist/main.fa80e48fc7541e9119ff.hot-update.js
@@ -0,0 +1,31 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateDiplom"]("main",{
+
+/***/ "./src/modules/burgerMenu.js":
+/*!***********************************!*\
+  !*** ./src/modules/burgerMenu.js ***!
+  \***********************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\src\\\\modules\\\\burgerMenu.js: 'Const declarations' require an initialization value. (7:10)\\n\\n\\u001b[0m \\u001b[90m  5 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  6 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m  7 |\\u001b[39m     window\\u001b[33m.\\u001b[39maddEventListener(\\u001b[32m'scroll'\\u001b[39m\\u001b[33m,\\u001b[39m () \\u001b[33m=>\\u001b[39m {\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m           \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  8 |\\u001b[39m         \\u001b[36mconst\\u001b[39m posMenuBtn \\u001b[33m=\\u001b[39m menuBtn\\u001b[33m.\\u001b[39mgetBoundingClientRect()\\u001b[33m.\\u001b[39mtop\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  9 |\\u001b[39m         console\\u001b[33m.\\u001b[39mlog(\\u001b[32m'posMenuBtn: '\\u001b[39m\\u001b[33m,\\u001b[39m posMenuBtn)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 10 |\\u001b[39m     })\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at Parser._raise (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:810:17)\\n    at Parser.raiseWithData (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:803:17)\\n    at Parser.raise (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:764:17)\\n    at Parser.parseVar (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13216:18)\\n    at Parser.parseVarStatement (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13026:10)\\n    at Parser.parseStatementContent (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12609:21)\\n    at Parser.parseStatement (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12542:17)\\n    at Parser.parseBlockOrModuleBlockBody (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13131:25)\\n    at Parser.parseBlockBody (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13122:10)\\n    at Parser.parseBlock (C:\\\\Portable program\\\\OSPanel\\\\domains\\\\localhost\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13106:10)\");\n\n//# sourceURL=webpack://Diplom/./src/modules/burgerMenu.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("f0879385aaaebbf4ec71")
+/******/ })();
+/******/ 
+/******/ }
+);
--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,9 +1,36 @@
 'use strict'
 
 const burgerMenu = () => {
+    const menuBtn = document.querySelector('.menu-button'),
+        topMenu = document.querySelector('.top-menu'),
+        posMenuVert = topMenu.getBoundingClientRect().top,
+        style = document.createElement('style');
 
+    const setFix = () => {
+        style.textContent = `
+        .top-menu {
+            position: fixed;
+        }
+        `;
+        document.head.appendChild(style);
+    };
 
-
-
+    const removeFix = () => {
+        style.textContent = `
+        .top-menu {
+            position: relative;
+        }
+        `;
+        document.head.appendChild(style);
+    };
+    const trackScroll = () => {
+        let scroll = window.pageYOffset;
+        if (scroll >= posMenuVert) {
+            setFix();
+        } else {
+            removeFix()
+        }
+    };
+    window.addEventListener('scroll', trackScroll);
 };
 export default burgerMenu;

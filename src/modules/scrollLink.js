6ac27ab0626b0d4ec04e848f6bc95fcbcc51f1e3
Konstantin Berezko <berezkok72@gmail.com>
2021-05-22 10:53:18 +0500

    fix(diplom): fix scrollBy

--- src/modules/scrollLink.js
+++ src/modules/scrollLink.js
@@ -2,8 +2,7 @@
 
 const scrollToLink = () => {
     const popMenu = document.querySelector('.popup-menu'),
-        topMenu = document.querySelector('.main-links'),
-        footerLogo = document.getElementById('footer-logo');
+        topMenu = document.querySelector('.main-links');
 
     // скролл на секцию
     const goLink = (href) => {
@@ -19,8 +18,6 @@ const scrollToLink = () => {
     // получение ссылки на секцию
     const addLink = (e, menuClass) => {
         const menu = document.querySelector(menuClass);
-        console.log('menu: ', menu);
-
         const idElem = e.target.href.split('#')[1];
         if (idElem) {
             const links = menu.querySelectorAll('a[href^="#"]');
@@ -37,22 +34,17 @@ const scrollToLink = () => {
     document.addEventListener('click', (e) => {
         if (e.target.closest('.popup-links')) {
             popMenu.style.display = 'none';
-            //const menu = document.querySelector('.popup-links');
             addLink(e, '.popup-links');
         }
-
         if (e.target.closest('.main-links')) {
             if (e.target === topMenu || e.target.matches('.scroll')) {
                 return;
             } else {
-                //const menu = document.querySelector('.main-links');
                 addLink(e, '.main-links');
             }
         }
-
         if (e.target.closest('.footer-left')) {
             if (e.target.matches('a[href]')) {
-                //const menu = document.querySelector('.footer-left');
                 addLink(e, '.footer-left');
             }
         }
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -5,18 +5,10 @@ import showPopupForm from './showForm';
 const sendForm = (form, inputs, statusMessage) => {
     const errorMessage = 'Произошла ошибка отправки. Попробуйте еще раз позже',
         loadMessage = 'Отправка...',
-        successMessage = 'Ваша заявка отправлена. Мы свяжемся с вами в ближайшее время.',
         loadColor = '#19b5fe',
         errorColor = '#fe193f',
         thanksForm = document.getElementById('thanks');
 
-    //создание базы данных
-    const formData = new FormData(form);
-    let body = {};
-
-    formData.forEach((item, key) => {
-        body[key] = item;
-    });
     // clear inputs
     const clearInputs = () => {
         inputs.forEach(item => {
@@ -32,20 +24,18 @@ const sendForm = (form, inputs, statusMessage) => {
         if (checkBox) {
             checkBox.checked = "false";
         }
-     };
-    const clearMessage = () => statusMessage.textContent = '';
+    };
 
     // сообщение ОК и очистка формы
-    const timing = 3000;
     const successResult = () => {
         const formElem = form.children;
         for (let i = 0; i < formElem.length - 1; i++) {
             formElem[i].removeAttribute('hidden');
         }
-        clearMessage();
+        statusMessage.textContent = '';
         clearInputs();
         const fullForm = form.closest('.popup');
-        if (form.closest('.popup')){
+        if (form.closest('.popup')) {
             fullForm.style.display = 'none';
         }
         showPopupForm(thanksForm);
@@ -54,7 +44,9 @@ const sendForm = (form, inputs, statusMessage) => {
     const showResult = (color, message) => {
         statusMessage.style.color = color;
         statusMessage.textContent = message;
-        setTimeout(clearMessage, timing);
+        setTimeout(() => {
+            statusMessage.textContent = '';
+        }, 3000);
     };
 
     const errorResult = (error) => {
@@ -62,15 +54,21 @@ const sendForm = (form, inputs, statusMessage) => {
             console.error(error);
         }
         if (form.id === 'banner-form') {
-            const messageForm = thanksForm.children[1].children[1].children[1];
+            const messageForm = document.getElementById('thanks-text');
             messageForm.textContent = errorMessage;
-            clearMessage();
+            statusMessage.textContent = '';
             showPopupForm(thanksForm);
         } else {
             showResult(errorColor, errorMessage);
         }
     };
 
+    //отправка данных на сервер
+    const formData = new FormData(form);
+    let body = {};
+    formData.forEach((item, key) => {
+        body[key] = item;
+    });
     const postData = (body) => {
         showResult(loadColor, loadMessage);
         return fetch('server.php', {

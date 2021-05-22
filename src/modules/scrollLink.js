1bbf3e7bf8183f8efa148bb48341e1fc84f6700b
Konstantin Berezko <berezkok72@gmail.com>
2021-05-22 10:15:32 +0500

    fix(diplom): fix scrollTo

--- src/modules/scrollLink.js
+++ src/modules/scrollLink.js
@@ -2,7 +2,8 @@
 
 const scrollToLink = () => {
     const popMenu = document.querySelector('.popup-menu'),
-        topMenu = document.querySelector('.main-links');
+        topMenu = document.querySelector('.main-links'),
+        footerLogo = document.getElementById('footer-logo');
 
     // скролл на секцию
     const goLink = (href) => {
@@ -16,13 +17,16 @@ const scrollToLink = () => {
         });
     };
     // получение ссылки на секцию
-    const addLink = (menu, e) => {
+    const addLink = (e, menuClass) => {
+        const menu = document.querySelector(menuClass);
+        console.log('menu: ', menu);
+
         const idElem = e.target.href.split('#')[1];
         if (idElem) {
             const links = menu.querySelectorAll('a[href^="#"]');
             links.forEach(item => {
                 let href = item.getAttribute('href').substring(1);
-                if (href === idElem) {
+                if (idElem === href) {
                     e.preventDefault();
                     goLink(href);
                 }
@@ -33,16 +37,23 @@ const scrollToLink = () => {
     document.addEventListener('click', (e) => {
         if (e.target.closest('.popup-links')) {
             popMenu.style.display = 'none';
-            const menu = document.querySelector('.popup-links');
-            addLink(menu, e);
+            //const menu = document.querySelector('.popup-links');
+            addLink(e, '.popup-links');
         }
 
         if (e.target.closest('.main-links')) {
             if (e.target === topMenu || e.target.matches('.scroll')) {
                 return;
             } else {
-                const menu = document.querySelector('.main-links');
-                addLink(menu, e);
+                //const menu = document.querySelector('.main-links');
+                addLink(e, '.main-links');
+            }
+        }
+
+        if (e.target.closest('.footer-left')) {
+            if (e.target.matches('a[href]')) {
+                //const menu = document.querySelector('.footer-left');
+                addLink(e, '.footer-left');
             }
         }
     });
